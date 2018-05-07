1a5860ec7645d32572ea4203d7a472503b822537
zydarChen <zydarChen@outlook.com>
2018-05-07 19:40:12 +0800

    update from zydar-ccnl

--- test.py
+++ test.py
@@ -152,27 +152,28 @@ def check(path='./PeMS/data/flow_data/87-N/405569/VDS405569-20160909-20160915.xl
 def check_all(path='./PeMS/data/flow_data'):
     cnt = 0
     for parent, _, file_names in os.walk(path):
-        for file_name in file_names:
+        for file_name in tqdm(file_names):
             cur_file = os.path.join(parent, file_name)
             if not check(cur_file):
                 cnt += 1
-                with open('./PeMS/data/error_file', 'a') as fw:
-                    fw.write(cur_file + '\n')
+                print(cnt)
+                # with open('./PeMS/data/error_file', 'a') as fw:
+                #     fw.write(cur_file + '\n')
     print(cnt)
 
 
 check_all()
-import time
-session = PeMS.download.login()
-with open('./PeMS/data/error_file', 'r') as fp:
-    for line in fp:
-        path = line.strip()
-        path_list = re.split(r'[/\\]', path)  # 同时处理Window与Linux路径
-        station_id = path_list[-2]
-        start = path_list[-1].split('-')[-2]
-        end = path_list[-1].split('-')[-1][:-5]
-        fwy = path_list[-3]
-        # print(fwy, station_id, start, end)
-        while not PeMS.download.get_data(start=start, end=end, station_id=int(station_id), fwy_name=fwy, session=session,
-                                         path='./PeMS/data/flow_data'):
-            print('Now is %s' % (time.strftime('%H:%M:%S', time.localtime())))
+# import time
+# session = PeMS.download.login()
+# with open('./PeMS/data/error_file', 'r') as fp:
+#     for line in fp:
+#         path = line.strip()
+#         path_list = re.split(r'[/\\]', path)  # 同时处理Window与Linux路径
+#         station_id = path_list[-2]
+#         start = path_list[-1].split('-')[-2]
+#         end = path_list[-1].split('-')[-1][:-5]
+#         fwy = path_list[-3]
+#         # print(fwy, station_id, start, end)
+#         while not PeMS.download.get_data(start=start, end=end, station_id=int(station_id), fwy_name=fwy, session=session,
+#                                          path='./PeMS/data/flow_data'):
+#             print('Now is %s' % (time.strftime('%H:%M:%S', time.localtime())))
